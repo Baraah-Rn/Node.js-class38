c77680ee11f30da82280da1c01a92d8fc8322184
Baraah-Rn <baraahranneh@gmail.com>
2022-08-08 13:30:58 +0200

    Adding Get request

---
+++ homework/hackyourtemperature/server.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const express = require("express");
+const port = 3000;
+
+
+const server = http.createServer((req, res)=>{
+  if(req.url === '/'){
+    res.write("hello from backend to frontend!");
+    res.end();
+  }
+});
+
+
+server.listen(port , () =>
+  console.log(`Example app listening at http://localhost:${port}`
+  ));
