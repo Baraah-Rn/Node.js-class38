eb9253f512e2a1f7cd81d29f725551b9abf206f2
Baraah-Rn <baraahranneh@gmail.com>
2022-08-15 17:11:04 +0200

    connect our API to an external API to grab the data we want

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -1,35 +1,10 @@
-import express from 'express';
-const port = process.env.port || 3000;
-const app = express();
-app.use(express.json());
-
-
-
-
-app.get('/', (req, res)=>{
-  if(req.url === '/'){
-    res.setHeader("Content-Type", "text/plain");
-    res.send("hello from backend to frontend!");
-    res.end();
-  }else{
-    alert("404... Something happened, PLease try again")
-  }
-});
-
-
-app.post('/weather', (req, res)=>{
-  const cityName = req.body.cityName
-  res.setHeader("Content-Type", "text/plain");
-    if(!cityName){
-      return res.send({message : "OOPS... Please enter City Name"})
-    }
-  res.send(cityName);
-  res.end();
-})
-
-
+import app from "./app.js";
+const port = process.env.port || 300
 
 
 app.listen(port , () =>
   console.log(`app listening at http://localhost:${port}`
   ));
+
+
+
---
+++ homework/hackyourtemperature/sources/keys.js
@@ -0,0 +1,3 @@
+export const keys = {
+  API_KEY: "5ed42efa9f449846d85b11da3ac26721",
+};
